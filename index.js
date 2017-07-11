7810910bac1b8f4e2ae44c3ef0a48a24d8cf5219
Xotic750 <xotic750@gmail.com>
2017-07-11 13:43:22 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,79 +1,48 @@
 /**
- * @file
- * <a href="https://travis-ci.org/Xotic750/assert-is-function-x"
- * title="Travis status">
- * <img
- * src="https://travis-ci.org/Xotic750/assert-is-function-x.svg?branch=master"
- * alt="Travis status" height="18">
- * </a>
- * <a href="https://david-dm.org/Xotic750/assert-is-function-x"
- * title="Dependency status">
- * <img src="https://david-dm.org/Xotic750/assert-is-function-x.svg"
- * alt="Dependency status" height="18"/>
- * </a>
- * <a
- * href="https://david-dm.org/Xotic750/assert-is-function-x#info=devDependencies"
- * title="devDependency status">
- * <img src="https://david-dm.org/Xotic750/assert-is-function-x/dev-status.svg"
- * alt="devDependency status" height="18"/>
- * </a>
- * <a href="https://badge.fury.io/js/assert-is-function-x" title="npm version">
- * <img src="https://badge.fury.io/js/assert-is-function-x.svg"
- * alt="npm version" height="18">
- * </a>
- *
- * If isFunction(callbackfn) is false, throw a TypeError exception.
- *
- * @version 1.2.0
+ * @file If isFunction(callbackfn) is false, throw a TypeError exception.
+ * @version 1.3.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module assert-is-function-x
  */
 
-/* eslint strict: 1 */
+'use strict';
 
-/* global require, module */
+var isFunction = require('is-function-x');
+var safeToString = require('safe-to-string-x');
+var isPrimitive = require('is-primitive');
 
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var isFunction = require('is-function-x');
-  var safeToString = require('safe-to-string-x');
-  var isPrimitive = require('is-primitive');
-
-  /**
-   * Tests `callback` to see if it is a function, throws a `TypeError` if it is
-   * not. Otherwise returns the `callback`.
-   *
-   * @param {*} callback The argument to be tested.
-   * @throws {TypeError} Throws if `callback` is not a function.
-   * @return {*} Returns `callback` if it is function.
-   * @example
-   * var assertIsFunction = require('assert-is-function-x');
-   * var primitive = true;
-   * var mySymbol = Symbol('mySymbol');
-   * var symObj = Object(mySymbol);
-   * var object = {};
-   * function fn () {}
-   *
-   * assertIsFunction(primitive);
-   *    // TypeError 'true is not a function'.
-   * assertIsFunction(object);
-   *    // TypeError '#<Object> is not a function'.
-   * assertIsFunction(mySymbol);
-   *    // TypeError 'Symbol(mySymbol) is not a function'.
-   * assertIsFunction(symObj);
-   *    // TypeError '#<Object> is not a function'.
-   * assertIsFunction(fn);
-   *    // Returns fn.
-   */
-  module.exports = function assertIsFunction(callback) {
-    if (!isFunction(callback)) {
-      var msg = isPrimitive(callback) ? safeToString(callback) : '#<Object>';
-      throw new TypeError(msg + ' is not a function');
-    }
-    return callback;
-  };
-}());
+/**
+ * Tests `callback` to see if it is a function, throws a `TypeError` if it is
+ * not. Otherwise returns the `callback`.
+ *
+ * @param {*} callback - The argument to be tested.
+ * @throws {TypeError} Throws if `callback` is not a function.
+ * @returns {*} Returns `callback` if it is function.
+ * @example
+ * var assertIsFunction = require('assert-is-function-x');
+ * var primitive = true;
+ * var mySymbol = Symbol('mySymbol');
+ * var symObj = Object(mySymbol);
+ * var object = {};
+ * function fn () {}
+ *
+ * assertIsFunction(primitive);
+ *    // TypeError 'true is not a function'.
+ * assertIsFunction(object);
+ *    // TypeError '#<Object> is not a function'.
+ * assertIsFunction(mySymbol);
+ *    // TypeError 'Symbol(mySymbol) is not a function'.
+ * assertIsFunction(symObj);
+ *    // TypeError '#<Object> is not a function'.
+ * assertIsFunction(fn);
+ *    // Returns fn.
+ */
+module.exports = function assertIsFunction(callback) {
+  if (!isFunction(callback)) {
+    var msg = isPrimitive(callback) ? safeToString(callback) : '#<Object>';
+    throw new TypeError(msg + ' is not a function');
+  }
+  return callback;
+};
--- tests/spec/test.js
+++ tests/spec/test.js
@@ -1,127 +1,126 @@
-/* jslint maxlen:80, es6:true, white:true */
+'use strict';
 
-/* jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
-   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
-   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-   es3:true, esnext:true, plusplus:true, maxparams:1, maxdepth:2,
-   maxstatements:11, maxcomplexity:3 */
-
-/* eslint strict: 1, max-lines: 1, symbol-description: 1, max-nested-callbacks: 1,
-   max-statements: 1 */
-
-/* global JSON:true, expect, module, jasmine, require, describe, xit, it,
-returnExports */
-
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var hasSymbolSupport = typeof Symbol === 'function' && typeof Symbol() === 'symbol';
-  var ifSymbolSupportIt = hasSymbolSupport ? it : xit;
-  var assertIsFunction;
-  if (typeof module === 'object' && module.exports) {
-    require('es5-shim');
-    require('es5-shim/es5-sham');
-    if (typeof JSON === 'undefined') {
-      JSON = {};
-    }
-    require('json3').runInContext(null, JSON);
-    require('es6-shim');
-    assertIsFunction = require('../../index.js');
-  } else {
-    assertIsFunction = returnExports;
+var hasSymbolSupport = typeof Symbol === 'function' && typeof Symbol('') === 'symbol';
+var ifSymbolSupportIt = hasSymbolSupport ? it : xit;
+var assertIsFunction;
+if (typeof module === 'object' && module.exports) {
+  require('es5-shim');
+  require('es5-shim/es5-sham');
+  if (typeof JSON === 'undefined') {
+    JSON = {};
   }
+  require('json3').runInContext(null, JSON);
+  require('es6-shim');
+  assertIsFunction = require('../../index.js');
+} else {
+  assertIsFunction = returnExports;
+}
 
-  describe('assertIsFunction', function () {
-    it('primitives should throw a TypeError', function () {
-      var block = function (value) {
-        try {
-          assertIsFunction(value);
-          return false;
-        } catch (e) {
-          expect(e).toEqual(jasmine.any(TypeError));
-          expect(e.message).toBe(String(value) + ' is not a function');
-        }
-        return true;
-      };
-      var values = [undefined, null, 1, true, ''];
-      var expected = values.map(function () {
-        return true;
-      });
-      var actual = values.map(block);
-      expect(actual).toEqual(expected);
-    });
-
-    it('objects should throw a TypeError', function () {
-      var block = function (value) {
-        try {
-          assertIsFunction(value);
-          return false;
-        } catch (e) {
-          expect(e).toEqual(jasmine.any(TypeError));
-          expect(e.message).toBe('#<Object> is not a function');
-        }
-        return true;
-      };
-      var values = [[], {}, /r/, new Date()];
-      var expected = values.map(function () {
-        return true;
-      });
-      var actual = values.map(block);
-      expect(actual).toEqual(expected);
-    });
-
-    ifSymbolSupportIt('Symbol literals should throw a TypeError', function () {
-      var block = function (value) {
-        try {
-          assertIsFunction(value);
-          return false;
-        } catch (e) {
-          expect(e).toEqual(jasmine.any(TypeError));
-          expect(e.message).toBe('Symbol(mySymbol) is not a function');
-        }
-        return true;
-      };
-      var values = [Symbol('mySymbol')];
-      var expected = values.map(function () {
-        return true;
-      });
-      var actual = values.map(block);
-      expect(actual).toEqual(expected);
-    });
-
-    ifSymbolSupportIt('Symbol objects should throw a TypeError', function () {
-      var block = function (value) {
-        try {
-          assertIsFunction(value);
-          return false;
-        } catch (e) {
-          expect(e).toEqual(jasmine.any(TypeError));
-          expect(e.message).toBe('#<Object> is not a function');
-        }
-        return true;
-      };
-      var values = [Object(Symbol('mySymbol'))];
-      var expected = values.map(function () {
-        return true;
-      });
-      var actual = values.map(block);
-      expect(actual).toEqual(expected);
-    });
-
-    it('should return the function', function () {
-      var block = function (value) {
-        try {
-          return assertIsFunction(value);
-        } catch (ignore) {}
+describe('assertIsFunction', function () {
+  it('primitives should throw a TypeError', function () {
+    var block = function (value) {
+      try {
+        assertIsFunction(value);
         return false;
-      };
-      var values = [function () {}, Array, block, assertIsFunction];
-      var expected = values.map(function (x) {
-        return x;
-      });
-      var actual = values.map(block);
-      expect(actual).toEqual(expected);
+      } catch (e) {
+        expect(e).toEqual(jasmine.any(TypeError));
+        expect(e.message).toBe(String(value) + ' is not a function');
+      }
+      return true;
+    };
+    var values = [
+      undefined,
+      null,
+      1,
+      true,
+      ''
+    ];
+    var expected = values.map(function () {
+      return true;
     });
+    var actual = values.map(block);
+    expect(actual).toEqual(expected);
   });
-}());
+
+  it('objects should throw a TypeError', function () {
+    var block = function (value) {
+      try {
+        assertIsFunction(value);
+        return false;
+      } catch (e) {
+        expect(e).toEqual(jasmine.any(TypeError));
+        expect(e.message).toBe('#<Object> is not a function');
+      }
+      return true;
+    };
+    var values = [
+      [],
+      {},
+      /r/,
+      new Date()
+    ];
+    var expected = values.map(function () {
+      return true;
+    });
+    var actual = values.map(block);
+    expect(actual).toEqual(expected);
+  });
+
+  ifSymbolSupportIt('Symbol literals should throw a TypeError', function () {
+    var block = function (value) {
+      try {
+        assertIsFunction(value);
+        return false;
+      } catch (e) {
+        expect(e).toEqual(jasmine.any(TypeError));
+        expect(e.message).toBe('Symbol(mySymbol) is not a function');
+      }
+      return true;
+    };
+    var values = [Symbol('mySymbol')];
+    var expected = values.map(function () {
+      return true;
+    });
+    var actual = values.map(block);
+    expect(actual).toEqual(expected);
+  });
+
+  ifSymbolSupportIt('Symbol objects should throw a TypeError', function () {
+    var block = function (value) {
+      try {
+        assertIsFunction(value);
+        return false;
+      } catch (e) {
+        expect(e).toEqual(jasmine.any(TypeError));
+        expect(e.message).toBe('#<Object> is not a function');
+      }
+      return true;
+    };
+    var values = [Object(Symbol('mySymbol'))];
+    var expected = values.map(function () {
+      return true;
+    });
+    var actual = values.map(block);
+    expect(actual).toEqual(expected);
+  });
+
+  it('should return the function', function () {
+    var block = function (value) {
+      try {
+        return assertIsFunction(value);
+      } catch (ignore) {}
+      return false;
+    };
+    var values = [
+      function () {},
+      Array,
+      block,
+      assertIsFunction
+    ];
+    var expected = values.map(function (x) {
+      return x;
+    });
+    var actual = values.map(block);
+    expect(actual).toEqual(expected);
+  });
+});
